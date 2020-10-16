9e0e3981cc70fe9ecaa7c628f81e83eb6375a654
Matt Reihing <52985513+mattreihing@users.noreply.github.com>
2020-10-16 00:09:37 -0500

    rearranged and updated js file

--- app.js
+++ app.js
@@ -5,17 +5,17 @@ const STORE = {
   // 5 or more questions are required
   questions: [
     {
-      question: 'What is the capital of Germany?',
+      questionTitle: 'What is the capital of Germany?',
       answers: [
         'Cologne',
         'Berlin',
         'Frankfurt',
         'Munich'
       ],
-      correctAnswer: '2' //or would it be 'Berlin'?
+      correctAnswer: 'Berlin' //or would it be '2'?
     },
     {
-      question: 'When was Berlin founded?',
+      questionTitle: 'When was Berlin founded?',
       answers: [
         'The 10th century',
         'The 13th century',
@@ -25,7 +25,7 @@ const STORE = {
       correctAnswer: 'The 13th century'
     },
     {
-      question: 'When was the Berlin Wall erected and torn down, respectively?',
+      questionTitle: 'When was the Berlin Wall erected and torn down, respectively?',
       answers: [
         '1934, 1991',
         '1949, 1991',
@@ -35,7 +35,7 @@ const STORE = {
       correctAnswer: '1949, 1991'
     },
     {
-      question: "What animal appears on Berlin's coat of arms?",
+      questionTitle: "What animal appears on Berlin's coat of arms?",
       answers: [
         'Bear',
         'Deer',
@@ -45,7 +45,7 @@ const STORE = {
       correctAnswer: 'Bear'
     },
     {
-      question: 'What river runs through Berlin?',
+      questionTitle: 'What river runs through Berlin?',
       answers: [
         'The Elbe',
         'The Donau',
@@ -80,29 +80,69 @@ const STORE = {
 
 // These functions return HTML templates
 
+
+/********** RENDER FUNCTION(S) **********/
+
+// This function conditionally replaces the contents of the <main> tag based on the state of the store
+
+//updateQuestionNumber
+// html could be something like: <h3>Question number <span> variableThatTracksNumber</span> </h3>
+
+//updateQuizScore
+
+/********** EVENT HANDLER FUNCTIONS 
+// These functions handle events (submit, click, etc) 
+**********/
+//=================================================================================================================================================================================================================================================================================
+
+
+
+
+// ***** PAGE 1 *****
 //renders the first page of the app
 function renderHomePage(){
-  const homePage = ` <img src="images/berlin.jpg" width="500" alt="Picture of Berlin's skyline with large soviet-era television tower in the center">
+  const homePage = `   <h2>5 Facts About Berlin You Were Afraid To Ask</h2>
+  <br>
+  <img src="images/berlin.jpg" width="500" alt="Picture of Berlin's skyline with large soviet-era television tower in the center">
   <!--Image by Claudio Schwarz -- @purzlbaum-->
   <br>
-  <p>5 Facts About Berlin You Were Afraid To Ask</p>
   <button type="submit" id="begin">Begin Quiz</button>`;
   $('main').html(homePage);
   console.log("renderHomePage has ran");
 }
 
-//renders the  after the quiz has started
+//event listener for the begin button
+function handleBeginButton(){
+  $('main').on('click', '#begin', function(event) {
+    console.log("begin button was clicked");
+    quizClickBegin();
+  });
+  console.log("handleBeginButton ran");
+};
+
+//begins the quiz and renders the first quiz page, setting quizStarted to true and questionNumber to 0
+function quizClickBegin(){
+  STORE.quizStarted = true; 
+  STORE.questionNumber = 0;
+  console.log(STORE.quizStarted); // true
+  console.log(STORE.questionNumber); // 0
+  renderQuizBegin();
+  renderQuestion();
+}
+
+
+// ***** QUIZ PAGE *****
+
+//renders the page after starting the quiz
 function renderQuizBegin(){
   const quizPage = `
-  
   <p>Question 1</p>
   <p>Total Correct: 0/5</p>
   <form>
     <label for="question" id="quiz-question"></label>
-    <fieldset id="choices></fieldset>
-      <ul>
-   
-     </ul>
+    <fieldset id="choices"></fieldset>
+      <ul id="question-items">
+      </ul>
       <input type="submit" value="SUBMIT"></input>
   </form>
   `
@@ -110,70 +150,39 @@ function renderQuizBegin(){
   console.log("renderQuizPage has ran");
 }
 
-/*
-<h2>{store.question}</h2>
-<ul>
-  then the li's are the question options contained in the array
-</ul>
-*/
-
-/********** RENDER FUNCTION(S) **********/
-
-// This function conditionally (if) replaces the contents of the <main> tag based on the state of the store // render/update the quiz questions here, but don't have to update the html
-
+//puts the question information from STORE into the DOM
 function renderQuestion(){
-  const question = STORE.questions[STORE.currentQuestion];
-  $('form label').text(question);
+  console.log(STORE.questions[STORE.questionNumber]);
+  //the above is grabbing all the information about the question, including the question, the answer choices, and the correct answer
+  console.log(STORE.questions);
+  //displaying all the objects within the array
   
-  /*$('#choices').html('');
-  STORE.questions.answers.forEach((answer,i)=>{
-    $('#choices').append(`
-      <input type="radio" name="choice" value="${i}" id="${i}"/>
-      <label for="${i}">${answer}</label>
-    `);
-  }); */
-  console.log("question has been rendered");
+  $('#quiz-question').text("why");
+  //no clue how to access the information in the STORE and have it display properly in the element, which is selected correctly.
+  
+  
+  console.log("render question has ran");
+  //isse with this function--question not appearing in the DOM correctly
 }
 
+/* 
 
+//updates the question number based on submit clicks as long as quizStarted = true
+function questionNumber(){
+  let questionNumberText = 0;
+  $('.js-question-number').text(questionNumberText);
+}
 
+//updates the quiz score based on how many correct answers have been selected
+function quizScore(){
+    
+}
+*/
 
+// ***** FINAL PAGE *****
 
-
-
-
-
-
-//updateQuestionNumber
-
-//updateQuizScore
-
-function quizClickBegin(){
-  STORE.quizStarted = true; 
-  //render first quiz page 
-  console.log(STORE.quizStarted);
-  renderQuizBegin();
-  renderQuestion()
-;}
-
-
-
-
-/********** EVENT HANDLER FUNCTIONS 
-// These functions handle events (submit, click, etc) 
-**********/
-
-
-
-//event listener for the begin button
-function handleBeginButton(){
-  $('main').on('click', '#begin', function(event) {
-    console.log("begin button was clicked");
-    quizClickBegin();
-  })
-  console.log("handleBeginButton ran");
-};
-
+//after all quiz questions have been answered, display how many correct answers have been selected and give the option to start the quiz again
+//quizStarted should get flipped back to false after answering last question(?) 
 
 
 //////////////////////////////////////////
